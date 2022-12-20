77e31028da3dca65d7d781fd6533fc3dfbe357bd
SHINSEOUNGROK <tnsfhr03@gmail.com>
2022-12-20 16:28:13 +0900

    #1.8 Todo 목록 기능 개발

--- src/main/java/org/zerock/springex/service/TodoService.java
+++ src/main/java/org/zerock/springex/service/TodoService.java
@@ -2,8 +2,13 @@ package org.zerock.springex.service;
 
 import org.zerock.springex.dto.TodoDTO;
 
+import java.util.List;
+
 
 public interface TodoService {
 
     void register(TodoDTO todoDTO);
+
+    List<TodoDTO> getAll();
+
 }
